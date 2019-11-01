e4001c6f364929cf40149b0da3d8ba71e8161819
Mulazi <36161774+Mulazi@users.noreply.github.com>
2019-11-01 19:06:20 +0700

    Add files via upload

---
+++ Modul4/Modul4/detail.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Modul4
+{
+    partial class detail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.plat_txt = new System.Windows.Forms.Label();
+            this.jenis_txt = new System.Windows.Forms.Label();
+            this.merk_txt = new System.Windows.Forms.Label();
+            this.thnbuat_txt = new System.Windows.Forms.Label();
+            this.harga_txt = new System.Windows.Forms.Label();
+            this.lain_txt = new System.Windows.Forms.Label();
+            this.tutup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(101, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DETAIL MOBIL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 111);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "No. Plat";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 148);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Jenis Mobil";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 188);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Merk Mobil";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 229);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tahun Buat";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 270);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Harga Sewa";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 311);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Lain-lain";
+            // 
+            // plat_txt
+            // 
+            this.plat_txt.AutoSize = true;
+            this.plat_txt.Location = new System.Drawing.Point(184, 111);
+            this.plat_txt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.plat_txt.Name = "plat_txt";
+            this.plat_txt.Size = new System.Drawing.Size(46, 17);
+            this.plat_txt.TabIndex = 7;
+            this.plat_txt.Text = "label8";
+            this.plat_txt.Click += new System.EventHandler(this.plat_txt_Click);
+            // 
+            // jenis_txt
+            // 
+            this.jenis_txt.AutoSize = true;
+            this.jenis_txt.Location = new System.Drawing.Point(184, 148);
+            this.jenis_txt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.jenis_txt.Name = "jenis_txt";
+            this.jenis_txt.Size = new System.Drawing.Size(46, 17);
+            this.jenis_txt.TabIndex = 8;
+            this.jenis_txt.Text = "label8";
+            // 
+            // merk_txt
+            // 
+            this.merk_txt.AutoSize = true;
+            this.merk_txt.Location = new System.Drawing.Point(184, 188);
+            this.merk_txt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.merk_txt.Name = "merk_txt";
+            this.merk_txt.Size = new System.Drawing.Size(46, 17);
+            this.merk_txt.TabIndex = 9;
+            this.merk_txt.Text = "label8";
+            // 
+            // thnbuat_txt
+            // 
+            this.thnbuat_txt.AutoSize = true;
+            this.thnbuat_txt.Location = new System.Drawing.Point(184, 229);
+            this.thnbuat_txt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.thnbuat_txt.Name = "thnbuat_txt";
+            this.thnbuat_txt.Size = new System.Drawing.Size(46, 17);
+            this.thnbuat_txt.TabIndex = 10;
+            this.thnbuat_txt.Text = "label8";
+            // 
+            // harga_txt
+            // 
+            this.harga_txt.AutoSize = true;
+            this.harga_txt.Location = new System.Drawing.Point(184, 270);
+            this.harga_txt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.harga_txt.Name = "harga_txt";
+            this.harga_txt.Size = new System.Drawing.Size(46, 17);
+            this.harga_txt.TabIndex = 11;
+            this.harga_txt.Text = "label8";
+            // 
+            // lain_txt
+            // 
+            this.lain_txt.AutoSize = true;
+            this.lain_txt.Location = new System.Drawing.Point(184, 311);
+            this.lain_txt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lain_txt.Name = "lain_txt";
+            this.lain_txt.Size = new System.Drawing.Size(46, 17);
+            this.lain_txt.TabIndex = 12;
+            this.lain_txt.Text = "label8";
+            // 
+            // tutup
+            // 
+            this.tutup.Location = new System.Drawing.Point(217, 368);
+            this.tutup.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tutup.Name = "tutup";
+            this.tutup.Size = new System.Drawing.Size(100, 28);
+            this.tutup.TabIndex = 13;
+            this.tutup.Text = "tutup";
+            this.tutup.UseVisualStyleBackColor = true;
+            this.tutup.Click += new System.EventHandler(this.tutup_Click);
+            // 
+            // detail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 444);
+            this.Controls.Add(this.tutup);
+            this.Controls.Add(this.lain_txt);
+            this.Controls.Add(this.harga_txt);
+            this.Controls.Add(this.thnbuat_txt);
+            this.Controls.Add(this.merk_txt);
+            this.Controls.Add(this.jenis_txt);
+            this.Controls.Add(this.plat_txt);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "detail";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kelompok 13";
+            this.Load += new System.EventHandler(this.detail_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label plat_txt;
+        private System.Windows.Forms.Label jenis_txt;
+        private System.Windows.Forms.Label merk_txt;
+        private System.Windows.Forms.Label thnbuat_txt;
+        private System.Windows.Forms.Label harga_txt;
+        private System.Windows.Forms.Label lain_txt;
+        private System.Windows.Forms.Button tutup;
+    }
+}
---
+++ Modul4/Modul4/detail.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modul4
+{
+    public partial class detail : Form
+    {
+        private string no_Plat;
+        private string jenis;
+        private string merk;
+        private string harga;
+        private string tahun_buat;
+        private string lain_lain;
+
+        public detail()
+        {
+            //Debug.WriteLine("detail form");
+            InitializeComponent();
+        }
+
+        public detail(string no_Plat, string jenis, string merk, string harga, string tahun_buat, string lain_lain)
+        {
+            InitializeComponent();
+            this.no_Plat = no_Plat;
+            this.jenis = jenis;
+            this.merk = merk;
+            this.harga = harga;
+            this.tahun_buat = tahun_buat;
+            this.lain_lain = lain_lain;
+        }
+
+       
+
+        private void detail_Load(object sender, EventArgs e)
+        {
+            this.plat_txt.Text = no_Plat;
+            this.jenis_txt.Text = jenis;
+            this.merk_txt.Text = merk;
+            this.harga_txt.Text = harga;
+            this.thnbuat_txt.Text = tahun_buat;
+            this.lain_txt.Text = lain_lain;
+
+        }
+
+        private void tutup_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("detail form");
+            this.Close();
+        }
+
+        private void plat_txt_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
